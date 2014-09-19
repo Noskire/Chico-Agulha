e48fe2a6079688b47bb5a295f9110e94c4de452c
Erikson Silacier <silacier2@hotmail.com>
2014-09-19 13:28:54 -0300

    Chico Agulha 0.2
    Adicionado movimento vertical, câmera segue personagem, animações se
    repetem. Pulo e limitações da área de movimentação do personagem estão
    conflitantes.

--- Assets/Scripts/ChicoScript.cs
+++ Assets/Scripts/ChicoScript.cs
@@ -4,33 +4,78 @@ using System.Collections;
 public class ChicoScript : MonoBehaviour {
 	public float maxSpeed;
 	public float maxImpulse;
+	public float gravidade;
 	public Animator animator;
+	int eixoX, eixoY;
+	float impulso, velocidade, yInicial;
+	bool pulando;
+	//public GameObject Chico;
 	//public GameObject projetile;
 
 	void Start(){ //Use this for initialization
 		animator = GetComponent<Animator>();
+		impulso = Time.deltaTime * maxSpeed;
+		velocidade = 0;
+		pulando = false;
+		//Chico = GetComponent<GameObject>();
 	}
 
 	void Update(){ //Update is called once per frame
 		//Instantiate(projetile);
-
-		//Movimento
+		eixoX = 0;
+		eixoY = 0;
+		//Movimento Vertical
+		if(Input.GetKey(KeyCode.W)){
+			eixoY = 1;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			eixoY = -1;
+		}
+		//Movimento Horizontal
 		if(Input.GetKey(KeyCode.A)){
-			transform.localScale = new Vector2(-1f, 1f);
-			rigidbody2D.velocity = new Vector2(-maxSpeed, rigidbody2D.velocity.y);
+			transform.localScale = new Vector3(-1f, 1f, 1f);
+			eixoX = -1;
 		}
 		if(Input.GetKey(KeyCode.D)){
-			transform.localScale = new Vector3(1f, 1f, 1f);
-			rigidbody2D.velocity = new Vector2(maxSpeed, rigidbody2D.velocity.y);
+			transform.localScale = new Vector3(1f, 1f, 1f);	
+			eixoX = 1;
 		}
-		animator.SetFloat("Speed", rigidbody2D.velocity.x);
-
+		transform.Translate(impulso * eixoX, impulso * eixoY, 0);
+		float f = 4.5f;
+		var pos = transform.position;
+		//Limitadores
+		/*if(pos.x < -f){
+			pos.x = -f;
+		}
+		if(pos.x > f){
+			pos.x = f;
+		}
+		if(pos.y < -0.6f){
+			pos.y = -0.6f;
+		}
+		if(pos.y > 1.5f){
+			pos.y = 1.5f;
+		}*/
+		if(eixoX != 0 || eixoY != 0){
+			animator.SetFloat ("Speed", impulso);
+		}else{
+			animator.SetFloat ("Speed", 0);
+		}
 		//Pulo
-		if(Input.GetKeyDown(KeyCode.Space)){
-			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, maxImpulse);
+		if(Input.GetKeyDown(KeyCode.Space) && !pulando){
+			pulando = true;
+			velocidade = maxImpulse;
+			yInicial = pos.y;
 		}
-		animator.SetFloat("Impulse", rigidbody2D.velocity.y);
-
+		if(pulando){
+			pos.y += velocidade;
+			velocidade += gravidade;
+			if(pos.y <= yInicial){
+				pulando = false;
+				pos.y = yInicial;
+			}
+		}
+		transform.position = pos;
 		//Defesa
 		if(Input.GetKey(KeyCode.J)){
 			animator.SetBool("Block", true);
@@ -38,7 +83,6 @@ public class ChicoScript : MonoBehaviour {
 		if(Input.GetKeyUp(KeyCode.J)){
 			animator.SetBool("Block", false);
 		}
-
 		//Ataque
 		if(Input.GetKeyDown(KeyCode.K)){
 			animator.SetBool("Attack", true);
