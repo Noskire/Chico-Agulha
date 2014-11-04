9bfbedd14206e04b0076736106c6909f5bad60b6
wandersonmedeiros <wanderson.medeiros2@gmail.com>
2014-11-04 11:40:46 -0300

    Orinetação a objeto de parte do projeto
    Augumas das classes do projeto.

---
+++ Assets/Scripts/AbsPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public abstract class AbsPlayer : AbsCharacter
+{
+    protected int mp;
+    protected bool jumping;
+    protected int xp;
+    protected int lvl;
+
+    public abstract void Start();
+    public abstract void Update();
+
+    public override void Walk()
+    {
+        this.transform.Translate(Vector2.right * this.velocityX * this.impulse);
+        this.animator.SetBool("Running", true);
+    }
+
+    public override void Fall()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Hit()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Die()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Attack()
+    {
+        animator.SetBool("Attacking", true);
+    }
+
+    public override void Attack2()
+    {
+
+        animator.SetBool("Attacking", true);
+        WeaponScript weapon = GetComponentInChildren<WeaponScript>();
+        if (weapon != null && weapon.CanAttack)
+        {
+            weapon.Attack(false);
+        }
+    }
+
+    public override void Block()
+    {
+        animator.SetBool("Defending", true);
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class Player :AbsPlayer
+{
+    public override void Start()
+    {
+        this.animator = this.gameObject.GetComponent<Animator>();
+        this.impulse = 0.2f;
+        this.velocityX = 0.2f;
+        this.velocityY = 0.2f; 
+    }
+
+    public override void Update()
+    {
+
+        //walk
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            this.transform.eulerAngles = new Vector2(0, 0);
+            this.Walk();
+
+        }
+        else
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                this.transform.eulerAngles = new Vector2(0, 180);
+                this.Walk();
+            }
+            else
+                this.animator.SetBool("Running", false);
+
+        //Jump
+        bool inputJ = Input.GetButtonDown("Jump");
+        var pos = this.transform.position;
+        if (inputJ && !this.jumping)
+        {
+            this.animator.SetBool("Jumping", true);
+            this.jumping = true;
+            this.velocityY = impulse;
+            this.yInitial = pos.y;
+        }
+
+        if (this.jumping)
+        {
+            pos.y += this.velocityY;
+            this.velocityY -= this.gravity;
+            if (pos.y <= this.yInitial)
+            {
+                this.jumping = false;
+                this.animator.SetBool("Jumping", false);
+                pos.y = this.yInitial;
+            }
+        }
+        this.transform.position = pos;
+
+
+        //Attack2
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.Attack2();
+        }
+        else
+        {
+            animator.SetBool("Attacking", false);
+        }
+
+        //block
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            this.Block();
+        }
+        else
+        {
+            animator.SetBool("Defending", false);
+        }
+
+    }
+}
+
