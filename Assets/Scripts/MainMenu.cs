244d79be5542e93d7645bd942edfe030b6649c58
Erikson Silacier <silacier2@hotmail.com>
2014-12-10 21:28:59 -0300

    Chico Agulha 3.2
    Menus (Inicial, Game Over e Level up) implementados.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour{
+	public Vector2 playGame;
+	public Vector2 loadGame;
+	public Vector2 options;
+	public Vector2 exit;
+	
+	private float buttonWidth = 0.3f;
+	private float buttonHeight = 0.1f;
+	
+	//private Texture texture; //Replace button form
+	//private GUIStyle style; //Stylize the button
+	private GUISkin skin;
+	
+	void Start(){
+		skin = Resources.Load("GUISkin") as GUISkin;
+	}
+	
+	void OnGUI(){
+		GUI.skin = skin;
+		if(GUI.Button(new Rect(Screen.width * playGame.x, Screen.height * playGame.y,
+		                       Screen.width * buttonWidth, Screen.height * buttonHeight), "Play Game")){
+			print("Clicked Play Game");
+			Application.LoadLevel("level1");
+		}		
+		if(GUI.Button(new Rect(Screen.width * loadGame.x, Screen.height * loadGame.y,
+		                       Screen.width * buttonWidth, Screen.height * buttonHeight), "Load Game")){
+			print("Clicked Load Game");
+		}		
+		if(GUI.Button(new Rect(Screen.width * options.x, Screen.height * options.y,
+		                       Screen.width * buttonWidth, Screen.height * buttonHeight), "Options")){
+			print("Clicked Options");
+		}		
+		if(GUI.Button(new Rect(Screen.width * exit.x, Screen.height * exit.y,
+		                       Screen.width * buttonWidth, Screen.height * buttonHeight), "Exit")){
+			Application.Quit();
+		}		
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,41 @@ using UnityEngine;
 
 public class Player:AbsPersonagem
 {
-    
+    //Atributos Basicos
     public int mp;
     public int mpMax;
-    public bool jumping;
+	public int exp;
+	public int expMax;
+	public int level;
+	public int skillPoints;
+
+	//Atributos que podem ser melhorados
+	public int meleeDamage;
+	public int rangedDamage;
+	//Nao esta aqui, mas pode ser melhorado
+		//hpMax
+		//velocidade
+
+	//Auxiliares
+	public bool jumping;
     public EnumEstado estadoPlayer;
     public bool isGrounded; // verifica se esta no solo
     public bool jumped; //verifica se esta pulando
     public float jumpTime; // usando para verificar se parou de pular
     public float force;	//forca do pulo
     public float jumpDelay; // tempo da animaçao
-    
 
-    public override void Andar()
+	//GUI
+	public bool showGameOver = false; // usado para mostrar o menu de Game Over
+	public bool showLevelUp = false; // usado para mostrar o menu de Level Up
+	private GUISkin skin; //Stylize the Gui
+
+	public Vector2 meleeDamagePosition;
+	public Vector2 rangedDamagePosition;
+	public Vector2 hpMaxPosition;
+	public Vector2 velocidadePosition;
+		
+	public override void Andar()
     {
         this.isGrounded = Physics2D.Raycast(this.transform.position, this.ground.position, 1 << LayerMask.NameToLayer("Plataforma"));
         this.animator.SetFloat("run", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
@@ -88,12 +110,94 @@ public class Player:AbsPersonagem
     {
         throw new System.NotImplementedException();
     }
+	
+	void OnGUI(){
+		const int buttonWidth = 120;
+		const int buttonHeight = 60;
+		GUI.skin = skin;
 
+		/* Game Over Menu Begin */
+		if(showGameOver){
+			// Center in X, 1/3 of the height in Y
+			if(GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2), (1 * Screen.height / 3) -
+			                       (buttonHeight / 2), buttonWidth, buttonHeight), "Retry!")){
+				// Reload the level
+				Application.LoadLevel("level1");
+			}
+			// Center in X, 2/3 of the height in Y
+			if(GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2), (2 * Screen.height / 3) -
+			                       (buttonHeight / 2), buttonWidth, buttonHeight), "Back to menu")){
+				// Reload the level
+				Application.LoadLevel("MainMenu");
+			}
+		}
+		/* Game Over Menu End */
 
+		/* Level Up Menu Begin */
+		/*meleeDamagePosition;
+		rangedDamagePosition;
+		hpMaxPosition;
+		velocidadePosition;*/
+		const float buttonWidth2 = 0.3f;
+		const float buttonHeight2 = 0.1f;
 
-    public override void Update()
+		if(showLevelUp){
+			// Center in X, 1/3 of the height in Y
+			if(GUI.Button(new Rect(Screen.width * meleeDamagePosition.x, Screen.height * meleeDamagePosition.y,
+			                       Screen.width * buttonWidth2, Screen.height * buttonHeight2), "Dano Fisico")){
+				if(skillPoints > 0){
+					meleeDamage++;
+					skillPoints--;
+				}else{
+					print("Voce nao tem pontos!");
+				}
+			}
+			if(GUI.Button(new Rect(Screen.width * rangedDamagePosition.x, Screen.height * rangedDamagePosition.y,
+			                       Screen.width * buttonWidth2, Screen.height * buttonHeight2), "Dano Baladeira")){
+				if(skillPoints > 0){
+					rangedDamage++;
+					skillPoints--;
+				}else{
+					print("Voce nao tem pontos!");
+				}
+			}
+			if(GUI.Button(new Rect(Screen.width * hpMaxPosition.x, Screen.height * hpMaxPosition.y,
+			                       Screen.width * buttonWidth2, Screen.height * buttonHeight2), "Vida")){
+				if(skillPoints > 0){
+					hpMax += 50;
+					hp += 50;
+					skillPoints--;
+				}else{
+					print("Voce nao tem pontos!");
+				}
+			}
+			if(GUI.Button(new Rect(Screen.width * velocidadePosition.x, Screen.height * velocidadePosition.y,
+			                       Screen.width * buttonWidth2, Screen.height * buttonHeight2), "Velocidade")){
+				if(skillPoints > 0){
+					velocidade++;
+					skillPoints--;
+				}else{
+					print("Voce nao tem pontos!");
+				}
+			}
+		}
+		/* Level Up Menu End */
+	}
+	
+	public override void Update()
     {
-        this.Andar();
+		if(hp <= 0){
+			showGameOver = true;
+		}
+
+		if(exp >= expMax){
+			exp -= expMax;
+			expMax *= 2;
+			level++;
+			skillPoints++;
+		}
+		//showLevelUp
+		this.Andar();
     }
 
 
@@ -110,6 +214,14 @@ public class Player:AbsPersonagem
         this.hp = 10;
         this.mpMax = 100;
         this.mp = 10;
+		this.expMax = 100;
+		this.exp = 0;
+		this.level = 1;
+		this.skillPoints = 0;
+		this.meleeDamage = 5;
+		this.rangedDamage = 5;
+
+		skin = Resources.Load("GUISkin") as GUISkin;
     }
 
 }
